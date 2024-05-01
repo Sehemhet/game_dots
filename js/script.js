9a0403ed0f6ad678d8c95d0d5bee79d2798a6865
Andrew Volk <andrewvolk@Andrews-MacBook-Air.local>
2024-05-01 16:41:47 +0300

    some again

--- js/script.js
+++ js/script.js
@@ -74,3 +74,31 @@ document.addEventListener('DOMContentLoaded', function() {
     var selectedButton = document.querySelector('#level_form button[data-tab="' + selectedTabId + '"]');
     showTab(selectedTabId, selectedButton);
 });
+
+
+function openPopup() {
+    var popup = document.querySelector('.popup');
+    if (popup) {
+        popup.style.display = 'flex';
+    }
+}
+
+function closePopup() {
+    var popup = document.querySelector('.popup');
+    if (popup) {
+        popup.style.display = 'none';
+    }
+}
+
+function openRaiting() {
+    var winnForm = document.querySelector('.winn_form');
+    var loseForm = document.querySelector('.lose_form');
+    var raitingForm = document.querySelector('.raiting_form');
+
+    if (winnForm.classList.contains('ACTIVE')) {
+        winnForm.classList.remove('ACTIVE');
+    } else if (loseForm.classList.contains('ACTIVE')) {
+        loseForm.classList.remove('ACTIVE');
+    }
+    raitingForm.classList.add('ACTIVE');
+}
